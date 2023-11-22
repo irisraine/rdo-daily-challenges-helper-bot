33a11e4dc0ea31be54a1c62d4ebe97ceb236ab12
Iris-Raine <iris.raine.darkest.star@gmail.com>
2023-11-22 19:51:58 +0000

    remove unnessesary else statement

--- engine/bot.py
+++ engine/bot.py
@@ -24,37 +24,37 @@ async def daily_challenges_guide():
     madam_nazar_location_message = messages.get_madam_nazar_location_message()
     if not tutorial_messages:
         logging.error(f"Данные о дейликах за {current_date} не отображены из-за ошибки")
+        return
+    await message_channel.send(
+        embed=header_messages['cover'].embed,
+        file=header_messages['cover'].image
+    )
+    await message_channel.send(
+        embed=header_messages['general'].embed,
+        file=header_messages['general'].image
+    )
+    for tutorial_message in tutorial_messages['general']:
+        await message_channel.send(
+            embed=tutorial_message.embed,
+            file=tutorial_message.image
+        )
+    await message_channel.send(
+        embed=header_messages['role'].embed,
+        file=header_messages['role'].image
+    )
+    for tutorial_message in tutorial_messages['role']:
+        await message_channel.send(
+            embed=tutorial_message.embed,
+            file=tutorial_message.image
+        )
+    if not madam_nazar_location_message:
+        logging.warning("Невозможно получить данные о местонахождении мадам Назар")
     else:
         await message_channel.send(
-            embed=header_messages['cover'].embed,
-            file=header_messages['cover'].image
+            embed=madam_nazar_location_message.embed,
+            file=madam_nazar_location_message.image
         )
-        await message_channel.send(
-            embed=header_messages['general'].embed,
-            file=header_messages['general'].image
-        )
-        for tutorial_message in tutorial_messages['general']:
-            await message_channel.send(
-                embed=tutorial_message.embed,
-                file=tutorial_message.image
-            )
-        await message_channel.send(
-            embed=header_messages['role'].embed,
-            file=header_messages['role'].image
-        )
-        for tutorial_message in tutorial_messages['role']:
-            await message_channel.send(
-                embed=tutorial_message.embed,
-                file=tutorial_message.image
-            )
-        if not madam_nazar_location_message:
-            logging.warning("Невозможно получить данные о местонахождении мадам Назар")
-        else:
-            await message_channel.send(
-                embed=madam_nazar_location_message.embed,
-                file=madam_nazar_location_message.image
-            )
-        logging.info(f"Данные о дейликах за {current_date} успешно отображены")
+    logging.info(f"Данные о дейликах за {current_date} успешно отображены")
 
 
 @client.event
